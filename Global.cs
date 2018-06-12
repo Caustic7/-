ee19379239a4021fa3a236cbd799d434277647e5
SadMeercat <igor9823@gmail.com>
2018-06-12 19:45:22 +0300

    Update Global.cs

--- Global.cs
+++ Global.cs
@@ -1,9 +1,15 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Global {
     public static float music;
     public static float sound;
-    public static float money;
+    public static int money = 0;
+    public static int hp = 3;
+    public static int atc = 30;
+    public static bool stick = false;
+    public static bool sword = false;
+    public static bool shield = false;
+    public static bool stout = false;
 }
